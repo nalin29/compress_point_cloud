a2fcd2303ed39c08756b1e7bcbf3cba4631e93cd
nalin29 <nalinmahajan@outlook.com>
2020-04-28 14:29:03 -0500

    Uses approx time instead and shows images

--- src/compress_node.cpp
+++ src/compress_node.cpp
@@ -1,4 +1,4 @@
-#include "ros/ros.h"
+#include <ros/ros.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
@@ -6,33 +6,52 @@
 #include <sensor_msgs/CameraInfo.h>
 #include <tf/tfMessage.h>
 #include <iostream>
-#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <cv_bridge/cv_bridge.h>
-#include <opencv2/videoio.hpp> 
+#include <image_transport/image_transport.h>
+#include <image_transport/subscriber_filter.h>
+#include <message_filters/sync_policies/approximate_time.h>
+class imageSub
+{  
+    public:
+    imageSub(ros::NodeHandle n, std::string &t1, std::string &t2) : it_(n), rgb_image(it_, t1, 1), depth_image(it_, t2, 1), sync(MySyncPolicy(10),rgb_image, depth_image)
+    {
+        sync.registerCallback(boost::bind(&imageSub::callback, this, _1, _2));
+    }
+    void callback(const sensor_msgs::ImageConstPtr &rgbImage, const sensor_msgs::ImageConstPtr &depthImage)
+    {
+        // compression methods run here should be moved to a sepeate cpp for running
+        cv_bridge::CvImagePtr cv_ptr1 = cv_bridge::toCvCopy(rgbImage, sensor_msgs::image_encodings::BGR8);
+        cv::Mat image1 = cv_ptr1->image;
+        cv_bridge::CvImagePtr cv_ptr2 = cv_bridge::toCvCopy(depthImage, sensor_msgs::image_encodings::TYPE_16UC1);
+        cv::Mat image2 = cv_ptr2->image;
+        ROS_INFO("RGB Image: %ld %ld", (long int)image1.rows, (long int)image1.cols);
+        ROS_INFO("Depth Image: %ld %ld", (long int)image2.rows, (long int)image2.cols);
+        if(!image1.data)
+            ROS_INFO("Incorrect data type for RGB");
+        if(!image2.data)
+            ROS_INFO("Incorrect data type for depth");
+        cv::imshow("RGB Image", image1);
+        cv::imshow("Depth Image", image2);
+        cv::waitKey(1);
+        //cv::VideoWriter rgbVideo("rbgOut.avi", CV_FOURCC('M', 'J', 'P', 'G'), 10, cv::Size(rgbImage->width, rgbImage->height));
+    }
 
-void callback(const sensor_msgs::ImageConstPtr& rgbImage, const sensor_msgs::CameraInfoConstPtr& rgbInfo, const sensor_msgs::ImageConstPtr& depthImage, const sensor_msgs::CameraInfoConstPtr& depthInfo){
-    // compression methods run here should be moved to a sepeate cpp for running
-    cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(rgbImage, sensor_msgs::image_encodings::BGR8);
-    cv::VideoWriter rgbVideo("rbgOut.avi", CV_FOURCC('M','J','P','G'), 10, cv::Size(rgbImage->width, rgbImage->height));
-}
-
-int main(int argc, char** argv){
+private:
+    image_transport::ImageTransport it_;
+    image_transport::SubscriberFilter rgb_image;
+    image_transport::SubscriberFilter depth_image;
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> MySyncPolicy;
+    message_filters::Synchronizer<MySyncPolicy> sync;
+};
+int main(int argc, char **argv)
+{
     ros::init(argc, argv, "compress_node");
+    std::string rgbImageNode = "/camera/rgb/image_color";
+    std::string depthImageNode = "/camera/depth/image_raw";
     ros::NodeHandle n;
-    std::string rgbImageNode = "camera/rgb/image_raw";
-    std::string rgbCamNode = "camera/rgb/camera_info";
-    std::string depthImageNode = "camera/depth/image_raw";
-    std::string depthCamNode = "camera/depth/camera_info";
-    std::string tfNode = "/tf";
-    n.param<std::string>("RGB_Image", rgbImageNode, "camera/rgb/image_raw");
-    n.param<std::string>("RGB_Cam", rgbCamNode, "camera/rgb/camera_info");
-    n.param<std::string>("Depth_Image", depthImageNode, "camera/depth/image_raw");
-    n.param<std::string>("Depth_Cam", depthCamNode, "camera/depth/camera_info");
-    n.param<std::string>("TF_Data", tfNode, "/tf");
-    message_filters::Subscriber<sensor_msgs::Image> rgb_image_sub(n, rgbImageNode, 1);
-    message_filters::Subscriber<sensor_msgs::CameraInfo> rgb_cam_info(n, rgbCamNode, 1);
-    message_filters::Subscriber<sensor_msgs::Image> depth_image(n, depthImageNode, 1);
-    message_filters::Subscriber<sensor_msgs::CameraInfo> depth_cam_info(n, depthCamNode, 1);
-    message_filters::TimeSynchronizer<sensor_msgs::Image, sensor_msgs::CameraInfo,sensor_msgs::Image, sensor_msgs::CameraInfo> sync(rgb_image_sub, rgb_cam_info, depth_image, depth_cam_info, 5);
-    sync.registerCallback(boost::bind(&callback, _1, _2, _3, _4));
+    imageSub im(n, rgbImageNode, depthImageNode);
+    ros::spin();
+    return 0;
 }
