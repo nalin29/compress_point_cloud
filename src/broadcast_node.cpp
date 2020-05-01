e65c84e0c3eb9c5fa12f416a3aa8a3b8c7b7c098
nalin29 <nalinmahajan@outlook.com>
2020-04-30 22:06:00 -0500

    Timed correctly

--- src/broadcast_node.cpp
+++ src/broadcast_node.cpp
@@ -15,15 +15,15 @@
 #include <std_msgs/String.h>
 
 class CompressBroadcaster {
-	ros::NodeHandle node;
 	image_transport::ImageTransport it;
 	image_transport::Publisher rgbVideo;
-
+	
 public:
-	CompressBroadcaster(): it(node){
+	CompressBroadcaster(ros::NodeHandle nh): it(nh){
 		rgbVideo = it.advertise("/camera/rgb/image_color", 1);
 	}
-	void publish(cv::Mat compressImage){
+	void publish(cv::Mat image){
+		cv::Mat compressImage = image;
 		cv_bridge::CvImage out_msg;
 		out_msg.header   = std_msgs::Header();
     	out_msg.encoding = sensor_msgs::image_encodings::BGR8;
@@ -32,17 +32,19 @@ public:
 	}
 };
 
-
 int main(int argc, char **argv) {
   ros::init(argc, argv, "broadcast_node");
-  cv::VideoCapture cap("rgbOut.avi");
-  int i = 0;
+  ros::NodeHandle nh;
+  cv::VideoCapture cap("RGBout.avi");
+  CompressBroadcaster cb(nh);
+  size_t i = 0;
   if (!cap.isOpened()){
   	std::cout << "no video" << std::endl;
   	return -1;
   }
-  CompressBroadcaster cb;
-  while (1){
+  // runs at rate of 30hz
+  ros::Rate rate(30.0);
+  while (cap.isOpened()){
   	cv::Mat frame;
   	cap >> frame;
   	std::cout << i++ << std::endl;
@@ -50,6 +52,8 @@ int main(int argc, char **argv) {
   		break;
   	}
   	cb.publish(frame);
+	// wait for 30hz rate
+	rate.sleep();
   }
   cap.release();
   ros::spin();
